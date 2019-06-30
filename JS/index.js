28fef0d889ac208d17fe282c97f5dcb186126b5b
Alagbe-Peter <alagbepeter63@gmail.com>
2019-06-30 06:20:01 +0100

    Final project worked on and presented

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+//$(document).ready((function){
+//    alert("READY");
+//})
+
+
+//          $(document).ready(function(){
+////                        alert("hello world");
+////              $('h1').hide();
+////              $('#rosy').hide();
+//              $('#b').click(function(){
+//                  alert("Good Day Class");
+//                    $("#s").hide();
+//              });
+//                            })
+      
+
+//document.getElementsByClassName("navbar-toggler").addEventListener("click", show);
